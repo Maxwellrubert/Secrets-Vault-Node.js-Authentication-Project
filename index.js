2558d16671b7d808f5eaecbcc58f91fdc3c2f6a0
Maxwell Rubert <maxwellrubert.cs22@stellamaryscoe.edu.in>
2026-09-18 12:31:17 +0530

    commit3

--- index.js
+++ index.js
@@ -56,18 +56,38 @@ app.get("/logout", (req, res) => {
   });
 });
 
-app.get("/secrets", (req, res) => {
+app.get("/secrets", async (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("secrets.ejs");
-
     //TODO: Update this to pull in the user secret to render in secrets.ejs
+    console.log(req.user);
+    try {
+      const result = await db.query("SELECT secret FROM users WHERE email = $1", [
+        req.user.email,
+      ]);
+      const userSecret = result.rows[0].secret;
+      if (userSecret) {
+        res.render("secrets.ejs", { secret: userSecret });
+      } else {
+        res.render("secrets.ejs", { secret: "No secret found or You have not submitted one yet." });
+      }
+    } catch (err) {
+      console.error("Error fetching secret:", err);
+    }
+  } else {
+    res.redirect("/login");
+  }
+}); 
+
+//TODO: Add a get route for the submit button
+//Think about how the logic should work with authentication.
+
+app.get("/submit", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("submit.ejs");
   } else {
     res.redirect("/login");
   }
 });
-
-//TODO: Add a get route for the submit button
-//Think about how the logic should work with authentication.
 
 app.get(
   "/auth/google",
@@ -128,6 +148,18 @@ app.post("/register", async (req, res) => {
 //TODO: Create the post route for submit.
 //Handle the submitted data and add it to the database
 
+app.post("/submit", async (req, res) => {
+  console.log(req.user);
+  const secret = req.body.secret;
+  try {
+    await db.query("UPDATE users SET secret = $1 WHERE email = $2", [secret, req.user.email]);
+    res.redirect("/secrets");
+  } catch (err) {
+    console.error("Error saving secret:", err);
+    res.redirect("/submit");
+  }
+});
+
 passport.use(
   "local",
   new Strategy(async function verify(username, password, cb) {
@@ -170,14 +202,14 @@ passport.use(
     },
     async (accessToken, refreshToken, profile, cb) => {
       try {
-        console.log(profile);
+        //console.log(profile);
         const result = await db.query("SELECT * FROM users WHERE email = $1", [
           profile.email,
         ]);
         if (result.rows.length === 0) {
           const newUser = await db.query(
             "INSERT INTO users (email, password) VALUES ($1, $2)",
-            [profile.email, "google"]
+            [profile.email, profile.id]
           );
           return cb(null, newUser.rows[0]);
         } else {
